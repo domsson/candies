64daf3fd33865e182548fab4b1b7f0352b6f8fca
domsson <julien.d.dau@gmail.com>
2019-04-06 23:33:00 +0200

    Implemented -u and -n and -s option for volume-pulse

--- volume-pulse/src/volume-pulse.c
+++ volume-pulse/src/volume-pulse.c
@@ -1,6 +1,25 @@
 #include <stdio.h>             // fprintf
 #include <stdlib.h>            // NULL, EXIT_*
+#include <unistd.h>            // getopt()
 #include <pulse/pulseaudio.h>
+
+struct candy_cfg {
+	pa_context *ctx;
+	pa_mainloop *mlp;
+	pa_mainloop_api *api;
+
+	int space : 1;
+	int unit : 1;
+	int precision;
+	char *muted;
+};
+
+void print_vol(double vol, int precision, int unit, int space)
+{
+	fprintf(stdout, "%.*f%s%s\n", precision, vol,
+			space && unit ? " " : "",
+			unit ? "%" : "");
+}
 
 /**
  * Callback for when we receive previously queried sink information.
@@ -14,19 +33,28 @@ void cb_sink_info(pa_context *c, const pa_sink_info *i, int eol, void *data)
 		return;
 	}
 	
+	struct candy_cfg *cfg = data;
+	
 	// Is the sink muted?
-	if (i->mute)
+	if (i->mute && cfg->muted)
 	{
-		// TODO
+		// Print the user-provided 'muted' string
+		fprintf(stdout, "%s\n", cfg->muted);
+
+		// We're done, let's quite main loop
+		pa_mainloop_quit(cfg->mlp, 0);
+		return;
 	}
 
-	// Get the sink volume, finally!
+	// Get the sink volume
 	pa_volume_t vol = pa_cvolume_avg(&i->volume);
-	char pretty_vol[16];
-	fprintf(stdout, "%s\n", pa_volume_snprint(pretty_vol, 16, vol));
+	double percent = vol / (PA_VOLUME_NORM / 100.0);
+
+	// Finally print the volume
+	print_vol(percent, cfg->precision, cfg->unit, cfg->space); 
 
 	// We're done, let's quit the main loop
-	pa_mainloop_quit(data, 0);
+	pa_mainloop_quit(cfg->mlp, 0);
 }
 
 /**
@@ -45,11 +73,8 @@ void cb_server_info(pa_context *c, const pa_server_info *i, void *data)
  */
 void cb_ctx_status(pa_context *c, void *data)
 {
-	// Get the state of the context
-	pa_context_state_t state = pa_context_get_state(c);
-
 	// If the state isn't ready, we've got nothing to do
-	if (state != PA_CONTEXT_READY)
+	if (pa_context_get_state(c) != PA_CONTEXT_READY)
 	{
 		return;
 	}
@@ -58,51 +83,92 @@ void cb_ctx_status(pa_context *c, void *data)
 	pa_context_get_server_info(c, cb_server_info, data);
 }
 
+void cleanup(struct candy_cfg *cfg)
+{
+	if (cfg->ctx != NULL)
+	{
+		pa_context_disconnect(cfg->ctx);
+		// TODO I'm not exactly sure what the next line does
+		// and if I should use it, but without it, we are leaking
+		// more memory according to valgrind... so...
+		pa_context_unref(cfg->ctx);
+	}
+	if (cfg->mlp != NULL)
+	{
+		pa_mainloop_free(cfg->mlp);
+	}
+}
+
 int main(int argc, char **argv)
 {
-	// Prepare all the important things
-	pa_context *ctx = NULL;
-	pa_mainloop *mlp = NULL;
-	pa_mainloop_api *api = NULL;
+	// Command line argument helpers
+	struct candy_cfg cfg = { 0 };
+	cfg.space = 1;
+
+	opterr = 0;
+	int o;
+	while ((o = getopt(argc, argv, "np:us:")) != -1)
+	{
+		switch(o)
+		{
+			case 'n':
+				cfg.space = 0;
+				break;
+			case 'p':
+				cfg.precision = atoi(optarg);
+				break;
+			case 'u':
+				cfg.unit = 1;
+				break;
+			case 's':
+				cfg.muted = optarg;
+				break;
+		}
+	}
 
 	// Get the mainloop object
-	if (!(mlp = pa_mainloop_new()))
+	if (!(cfg.mlp = pa_mainloop_new()))
 	{
-		fprintf(stderr, "pa_mainloop_new() failed.\n");
+		//fprintf(stderr, "pa_mainloop_new() failed.\n");
 		return EXIT_FAILURE;
 	}
 
 	// Get the mainloop API object
-	api = pa_mainloop_get_api(mlp);
+	cfg.api = pa_mainloop_get_api(cfg.mlp);
 
 	// Get the context object
-	if (!(ctx = pa_context_new(api, NULL)))
+	if (!(cfg.ctx = pa_context_new(cfg.api, NULL)))
 	{
-		fprintf(stderr, "pa_context_new() failed.\n");
+		//fprintf(stderr, "pa_context_new() failed.\n");
+		cleanup(&cfg);
 		return EXIT_FAILURE;
 	}
 
 	// Set the func to call on context state change
-	pa_context_set_state_callback(ctx, cb_ctx_status, mlp);
+	pa_context_set_state_callback(cfg.ctx, cb_ctx_status, &cfg);
 
 	// Connect the context to the pulse server
 	pa_context_flags_t flags = { 0 };
-	if (pa_context_connect(ctx, NULL, flags, NULL) < 0)
+	if (pa_context_connect(cfg.ctx, NULL, flags, NULL) < 0)
 	{
-		fprintf(stderr, "pa_context_connect() failed.\n");
+		//fprintf(stderr, "pa_context_connect() failed.\n");
+		cleanup(&cfg);
 		return EXIT_FAILURE;
 	}
 
+	// Turn off buffering for stdout
+	setbuf(stdout, NULL);
+
 	// Run the main loop
-	if (pa_mainloop_run(mlp, NULL) < 0)
+	if (pa_mainloop_run(cfg.mlp, NULL) < 0)
 	{
-		fprintf(stderr, "pa_mainloop_run() failed.\n");
+		//fprintf(stderr, "pa_mainloop_run() failed.\n");
+		cleanup(&cfg);
 		return EXIT_FAILURE;
 	}
 
 	// Disconnect, close and free things
-	pa_context_disconnect(ctx);
-	pa_mainloop_free(mlp);
+	cleanup(&cfg);
 
 	return EXIT_SUCCESS;
 }
