b0df4ebcdf2df299a40c32db94e5cfc2dcebe3d5
domsson <julien.d.dau@gmail.com>
2019-11-24 18:36:01 +0900

    Add a memory usage candy, using sysinfo() (Linux only)

---
+++ mem-sysinfo/README.md
@@ -0,0 +1,31 @@
+# mem-sysinfo 
+
+This is a small utility that tries to print current memory usage to `stdout`.
+It is intended to be used with status bars like `Lemonbar` and/or their 
+wrappers, like [`succade`](https://github.com/domsson/succade).
+
+## Concept 
+
+The tool gets information about the amount of total and free memory via 
+`sysinfo()`.
+
+## Dependencies
+
+None.
+
+## Building
+
+- Make sure `gcc` are installed
+- Run the included `build` script
+
+## Usage
+
+    mem-sysinfo [-h] [-i INTERVAL] [-m] [-p NUM] [-s] [-u]
+
+- `-h` print usage information, then exit
+- `-i` time between reads from `/proc/stat` and therefore between prints, in seconds
+- `-m` keep running and printing every second (or every INTERVAL seconds)
+- `-p` precision: number of decimals to include in the output
+- `-s` print a space between the value and unit
+- `-u` add the percentage sign (`" %"`) to the output
+
---
+++ mem-sysinfo/src/mem-sysinfo.c
@@ -0,0 +1,116 @@
+#include <stdio.h>            // fprintf
+#include <stdlib.h>           // NULL, EXIT_*
+#include <unistd.h>           // getopt() et al.
+#include <string.h>           // strstr()
+#include <sys/sysinfo.h>      // sysinfo()
+
+#define DEFAULT_UNIT    "%"
+#define DEFAULT_INTERVAL 1
+
+void print_usage(double usage, int precision, const char *unit, int space)
+{
+	fprintf(stdout, "%.*lf%s%s\n", precision, usage,
+			space && strlen(unit) ? " " : "", unit);
+}
+
+int get_mem_info(long *total, long *free)
+{
+	struct sysinfo info = { 0 };
+	if (sysinfo(&info) == -1)
+	{
+		return -1;
+	}
+
+	*total = info.totalram;
+	*free  = info.freeram;
+	return 0;
+}
+
+/**
+ * Prints usage information.
+ */
+void help(char *invocation)
+{
+	fprintf(stderr, "Usage:\n");
+     	fprintf(stderr, "\t%s [OPTION...] [-f PROCFILE] [-i INTERVAL] [-p PRECISION]\n", invocation);
+	fprintf(stderr, "\n");
+	fprintf(stderr, "Options:\n");
+	fprintf(stderr, "\t-h Print this help text and exit.\n");
+	fprintf(stderr, "\t-m Keep running and printing every second (or INTERVAL seconds).\n"); 
+	fprintf(stderr, "\t-u Print a percentage sign after the CPu usage value.\n");
+	fprintf(stderr, "\t-s Print a space between CPU usage and percentage sign.\n");
+}
+
+int main(int argc, char **argv)
+{
+	// TODO: implement threshold option, so that we only print
+	//       again once the memory usage has changed by the given 
+	//       percentage, instead of using a set interval
+	//
+	// TODO: implement the option to show absolute values instead 
+	//       of a percentage
+	//
+	// TODO: maybe implement the option to show FREE memory instead
+	//       of USED memory; not sure about this one
+	
+	int monitor = 0;        // keep running and printing
+	int interval = 0;       // print every `interval` seconds
+	int unit = 0;		// also print the % unit
+	int space = 0;          // space between val and unit
+	int precision = 0;      // decimal places in output
+
+	// Get arguments, if any
+	opterr = 0;
+	int o;
+	while ((o = getopt(argc, argv, "mi:p:ush")) != -1)
+	{
+		switch (o)
+		{
+			case 'm':
+				monitor = 1;
+				break;
+			case 'i':
+				interval = atoi(optarg);
+				break;
+			case 'p':
+				precision = atoi(optarg);
+				break;
+			case 'u':
+				unit = 1;
+				break;
+			case 's':
+				space = 1;
+				break;
+			case 'h':
+				help(argv[0]);
+				return EXIT_SUCCESS;
+		}
+	}
+
+	if (interval == 0)
+	{
+		interval = DEFAULT_INTERVAL;
+	}
+	
+	// Disable stdout buffering
+	setbuf(stdout, NULL);
+
+	long total   = 0;
+	long free    = 0;
+	double usage = 0;
+
+	do
+	{
+		get_mem_info(&total, &free);
+		usage = (1 - ((double) free / (double) total)) * 100;
+		print_usage(usage, precision, unit ? DEFAULT_UNIT : "", space);
+		if (monitor)
+		{
+			sleep(interval);
+		}
+	}
+	while (monitor);
+
+	return EXIT_SUCCESS;
+}
+
